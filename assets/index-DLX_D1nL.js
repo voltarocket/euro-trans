989e118658c9712e9bce7c20fd6911e9fb1b6da6
voltarocket <voltarocket@gmail.com>
2025-03-30 01:28:24 +0300

    Updates

---
+++ assets/index-DLX_D1nL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BevdKaQ2.js";const o=()=>r.jsx("div",{children:"Hello"});export{o as default};
---
+++ assets/index-Dghxtdyf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BevdKaQ2.js";const t=()=>e.jsx("div",{children:"Hello"});export{t as default};
